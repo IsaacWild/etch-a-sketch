d6e218cd8316def2df34b3588089a60f6c5d010b
Isaac <isaacwildsecond@gmail.com>
2022-03-07 17:57:09 +0000

    made grid with boxes that change colour when moused over

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const gridWrapper = document.querySelector(".gridWrapper")
 const box = document.createElement("div");
 // gridWrapper.appendChild(box);
 box.classList.add("box");
-
-function makeRow(x){
+           
+function makeGrid(x){
     if(x>100){
         alert("Please pick a smaller number")
     }else
@@ -15,8 +15,28 @@ function makeRow(x){
             const box2 = document.createElement("div");
             box.appendChild(box2);
             box2.classList.add("box2");
+            box2.addEventListener("mouseenter", () =>{
+                box2.classList.add("box2Colour");
+                box2.classList.remove("box2")
+            })
         }
     }
 }
 
-makeRow(16);
+makeGrid(10);
+
+
+
+// this only made the 1st box to change colour when mouse entered any box2
+// const boxes = document.querySelectorAll(".box2");
+
+// boxes.forEach(boxes =>{
+//     boxes.addEventListener("mouseover", (e)=>{
+//         document.querySelector(".box2").style.background = "red"
+//     })
+// })
+
+// document.querySelector(".box2").addEventListener("mouseover", () => {
+//     document.querySelector(".box2").style.background = "red"
+// })
+
