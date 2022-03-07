3182e11b924791f906a85ef4c0ea6fa6a06ae288
Isaac <isaacwildsecond@gmail.com>
2022-03-07 17:17:10 +0000

    Have function and Styling to create a grid of boxes

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const gridWrapper = document.querySelector(".gridWrapper")
+const box = document.createElement("div");
+// gridWrapper.appendChild(box);
+box.classList.add("box");
 
+function makeRow(x){
+    if(x>100){
+        alert("Please pick a smaller number")
+    }else
+    for(i=0; i<x; i++){
+        const box = document.createElement("div");
+        gridWrapper.appendChild(box);
+        box.classList.add("box");
+        for(j=0; j<x; j++){
+            const box2 = document.createElement("div");
+            box.appendChild(box2);
+            box2.classList.add("box2");
+        }
+    }
+}
 
-
+makeRow(16);
